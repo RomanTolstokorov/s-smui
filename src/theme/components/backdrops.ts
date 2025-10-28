80a72413e1df671c9aa96dcfb94222d23adaa12f
Roman.Tolstokorov <seftoner@gmail.com>
2025-10-28 16:41:02 +0200

    refactor: Comment out unused root style overrides in backdrops component

--- src/theme/components/backdrops.ts
+++ src/theme/components/backdrops.ts
@@ -3,10 +3,10 @@ import type {} from '@mui/material/themeCssVarsAugmentation';
 export const backdropsOverrides = {
 MuiBackdrop: {
       styleOverrides: {
-        root: ({ theme }: { theme: any }) => ({
+        // root: ({ theme }: { theme: any }) => ({ 
         //   backgroundColor: theme.vars.palette.components.backdrop.default,
         //   backdropFilter: 'blur(1px)',
-        }),
+        // }),
       },
     },
 }
